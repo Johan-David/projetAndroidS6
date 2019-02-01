a405a59e93b26cc50feb6fc4dc9182c1c7e43b39
bdarmet <benjamin.darmet@etu.u-bordeaux.fr>
2019-02-01 17:38:01 +0100

    accés à la caméra + menu déroulant

--- app/src/main/java/com/example/jdavid004/projetandroids6/MainActivity.java
+++ app/src/main/java/com/example/jdavid004/projetandroids6/MainActivity.java
@@ -1,28 +1,58 @@
 package com.example.jdavid004.projetandroids6;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
-
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView img;
+    Picture picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         img = findViewById(R.id.vegetablePicture);
-        Picture picture = new Picture(getResources());
+        this.picture = new Picture(getResources());
         img.setImageBitmap(picture.getBmp());
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            // Cas où on clique sur la caméra pour accéder à l'appareil photo.
+            case R.id.camera:
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivityForResult(intent,0);
+                return true;
+            case R.id.toGrey:
+                picture.toGray(picture.getBmp());
+            case R.id.contrastMenu:
+                Toast.makeText(this,"menu selected", Toast.LENGTH_SHORT);
+                return true;
 
+            case R.id.convolutionMenu:
+                Toast.makeText(this,"menu selected", Toast.LENGTH_SHORT);
+                return true;
 
-
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
