0fbc44dc020a699fe8c8fe05834ca309f55e6db7
bdarmet <benjamin.darmet@etu.u-bordeaux.fr>
2019-02-01 13:51:37 +0100

    Création classe convolution

--- app/src/main/java/com/example/jdavid004/projetandroids6/MainActivity.java
+++ app/src/main/java/com/example/jdavid004/projetandroids6/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.jdavid004.projetandroids6;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v8.renderscript.RenderScript;
-import android.support.v8.renderscript.Allocation;
 import android.widget.ImageView;
 
-import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
